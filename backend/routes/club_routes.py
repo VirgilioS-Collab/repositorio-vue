48b24ba1486a34e20b990fb180865c96bc60ebbb
Angel Chon <Angzbchon@hotmail.com>
2025-06-29 14:17:55 -0500

    Endpoints solicitado

---
+++ backend/routes/club_routes.py
@@ -0,0 +1,16 @@
+"""
+Club Routes
+Define las rutas para los clubs/grupos
+"""
+from flask import Blueprint
+from controllers.club_controller import (
+    get_club_details,
+    update_club_settings
+)
+
+# Crear blueprint
+club_bp = Blueprint("club_bp", __name__)
+
+# Rutas para clubs/grupos
+club_bp.route("/api/groups/<int:club_id>", methods=['GET'])(get_club_details)
+club_bp.route("/api/admin/clubs/<int:club_id>/settings", methods=['PUT'])(update_club_settings)
