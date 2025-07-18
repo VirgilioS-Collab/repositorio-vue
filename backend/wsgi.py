1c1e67e4c3bf19c92f19030cdd9464a8f29f9d4e
carroyo <arroyocristhian8@gmail.com>
2025-07-17 20:35:59 -0500

    Nuevos cambios

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -3,7 +3,4 @@ Este archivo permite que Gunicorn inicie correctamente el worker asociado a la a
 """
 
 from app import app
-from listener import start_listener
 import threading
-
-threading.Thread(target=start_listener, daemon=True).start()
